39d2411b337b5e34f1922f28979e67a88105cda1
blendid3 <zhqblendid@gmail.com>
2019-12-01 22:53:34 -0800

    server_try is what I do

--- src/server_try.py
+++ src/server_try.py
@@ -19,9 +19,18 @@ logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s',)
 
 ### parameter
-term=0
-log=[]
+currentTerm = 0
+votedFor = None
+id=0##for example
+log = []  ##[term, index,command]
 commitIndex = 0## current log index
+lastApplied = 0  ## applied in state machine
+nextIndex = [0,0,0,0,0] ##
+matchIndex = [0,0,0,0,0] ## the value is matchIndex 2,3,1,2,3, for example, if the leader is 0, then it means fellower 1 have replicated 3 entries
+## and the fellower 2 have replicated 1 entires
+
+
+
 flag_follower=False
 flag_leader=False
 flag_crash=False
@@ -114,27 +123,72 @@ class _thread_leader_fellower (threading.Thread):
                 candidate_action()
 
     def leader_action(self):
-        time_signal = threading.Event()
-        while flag_leader:
-            leader_threads = []
+        global nextid
+        global match_index
+        nextid = [commitIndex + 1, commitIndex + 1, commitIndex + 1, commitIndex + 1, commitIndex + 1]
+        match_index = [0, 0, 0, 0, 0]
+
+        leader_threads = {}
+        time_signals = {}
+        for follower in followers:
+            prevlogindex = nextid[follower[0]] - 1
+            prev_log = log[prevlogindex]
+            follower_signal = threading.Event()
+            time_signals[follower[0]] = follower_signal
+            if nextid[follower[0]] >= len(log):
+                entries = []
+            else:
+                entries = log[nextid[follower[0]]]
             thread_append = threading.Thread(name='periodically append',
                                              target=append_entries_to_follower,
-                                             args=(time_signal,))
+                                             args=(follower_signal, follower, prevlogindex, prev_log, entries, currentTerm))
+            thread_append.setDaemon(True)
             thread_append.start()
-            leader_threads.append(thread_append)
-            thread_control = Timer(period_heartbeat, leader_signal_periodically)
+            thread_append_follower = []
+            thread_append_follower.append(thread_append)
+            thread_control = threading.Timer(period_heartbeat, leader_signal_periodically, (follower_signal,))
+            thread_control.setDaemon(True)
             thread_control.start()
-            leader_threads.append(thread_control)
-            for thread in leader_threads:
-                thread.join()
+            thread_append_follower.append(thread_control)
+            leader_threads[follower[0]] = thread_append_follower
+            ## leader_threads is the list of [thread_append, thread_control]
+
+        while flag_leader:
+            pass
+
 
     def leader_signal_periodically(self,time_signal):
-        time_signal.set()
 
-    def append_entries_to_follower(self,time_signal):
+        time_signal.set()
+        threading.Timer(period_heartbeat, leader_signal_periodically, (follower_signal,)).start()
+
+    def append_entries_to_follower(self,time_signal,follower,prevlogindex, prev_log,entries,leader_term ):
+        global flag_leader
+        global nextid
+        global match_index
         time_signal.wait()
-        append_entries()
+        follower_rpc = follower[1]
+        flag,match_index,follower_term=follower_rpc.append_Entries(prevlogindex, prev_log, entrie, leader_term)
         time_signal.clear()
+        ###judge the case:
+        if leader_term <follower_term:
+            flag_leader=0
+            flag_follower=1
+            global currentTerm
+            currentTerm=follower_term
+            return
+
+        if flag==False:
+            nextid[follower[0]]=nextid[follower[0]] - 1
+        else:
+            match_index[follower[0]]=match_index
+        ##
+
+        prevlogindex = nextid[follower[0]] - 1
+        prev_log = log[prevlogindex]
+        self.append_entries_to_follower(time_signal, follower, prevlogindex, prev_log, entries, leader_term)
+
+
 
     def timeout(self):
         flag_candidate = 1
@@ -160,3 +214,25 @@ def crash():
     flag_leader=0
     flag_follower=0
     flag_candidate=0
+
+
+
+
+def append_Entries(prevlogindex, prev_log, entries, leader_term):
+        if leader_term == term:
+            if log[prevlogindex] != prev_log:
+                match_index = 0
+                return False, match_index, term
+            if entries:  ## have the entries value
+                if len(log) - 1 == prevlogindex:
+                    log.append(entries)
+                else:
+                    log[prevlogindex + 1] = entries
+                    while len(log) > prevlogindex + 2:
+                        log.pop()
+                match_index = prevlogindex + 1
+                return True, match_index, term
+            else:  ## is a heartbeat
+                reset_timer = 1  ##reset the timer
+                return True, match_index, term
+
