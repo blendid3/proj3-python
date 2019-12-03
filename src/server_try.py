24784e405b3c13cbcbd3b91b3d52507c36f9f5a1
blendid3 <zhqblendid@gmail.com>
2019-12-03 10:05:01 -0800

    Update server_try.py

--- src/server_try.py
+++ src/server_try.py
@@ -18,27 +18,152 @@ class threadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s',)
 
-### parameter
-currentTerm = 0
-votedFor = None
-id=0##for example
-log = []  ##[term, index,command]
-commitIndex = 0## current log index
-lastApplied = 0  ## applied in state machine
-nextIndex = [0,0,0,0,0] ##
-matchIndex = [0,0,0,0,0] ## the value is matchIndex 2,3,1,2,3, for example, if the leader is 0, then it means fellower 1 have replicated 3 entries
-## and the fellower 2 have replicated 1 entires
+##only check the fileinfomap
+
+
+###rpc function
+###
+def ping():
+    """A simple ping method"""
+    print("Ping()")
+    return True
+
+# Gets a block, given a specific hash value
+def getblock(h):
+    """Gets a block"""
+    print("GetBlock(" + h + ")")
+
+    blockData = bytes(4)
+    return blockData
+
+# Puts a block
+def putblock(b):
+    """Puts a block"""
+    print("PutBlock()")
+
+    return True
+
+# Given a list of hashes, return the subset that are on this server
+def hasblocks(hashlist):
+    """Determines which blocks are on this server"""
+    print("HasBlocks()")
+
+    return hashlist
+
+# Retrieves the server's FileInfoMap
+def getfileinfomap():
+    """Gets the fileinfo map"""
+    print("GetFileInfoMap()")
+
+    return fileinfomap
+
+# Update a file's fileinfo entry
+def updatefile(filename, version, hashlist):
+    """Updates a file's fileinfo entry"""
+    print("UpdateFile("+filename+")")
+
+    fileinfomap[filename] = [version, hashlist]
+
+    return True
+
+# PROJECT 3 APIs below
+
+# Queries whether this metadata store is a leader
+# Note that this call should work even when the server is "crashed"
+def isLeader():
+    """Is this metadata store a leader?"""
+
+    print("IsLeader()")
+    if flag_leader == 0:
+        return True
+
+# "Crashes" this metadata store
+# Until Restore() is called, the server should reply to all RPCs
+# with an error (unless indicated otherwise), and shouldn't send
+# RPCs to other servers
+def crash():
+    """Crashes this metadata store"""
+    print("Crash()")
+    global flag_candidate
+    global flag_leader
+    global flag_follower
+    global flag_crash
+    flag_crash=1
+    flag_follower=0
+    flag_leader=0
+    flag_candidate=0
+    return True
+
+# "Restores" this metadata store, allowing it to start responding
+# to and sending RPCs to other nodes
+def restore():
+    """Restores this metadata store"""
+    print("Restore()")
+    global flag_candidate
+    global flag_leader
+    global flag_follower
+    global flag_crash
+    flag_crash=0
+    flag_follower=1
+    flag_leader=0
+    flag_candidate=0
+    return True
+
+
+# "IsCrashed" returns the status of this metadata node (crashed or not)
+# This method should always work, even when the node is crashed
+def isCrashed():
+    """Returns whether this node is crashed or not"""
+    print("IsCrashed()")
+    if flag_crash==1:
+        return True
+
+# Requests vote from this server to become the leader
+def requestVote(serverid, candidate_term,candidate_log,candidate_prevlogindex):
+    """Requests vote to be the leader"""
+    global currentTerm
+    ## that is because candidate term must larger than follower, or it cannot get the vote from the follower
+    if candidate_term>currentTerm:
+        currentTerm=candidate_term
+        if log[candidate_prevlogindex]==log[commitIndex]:## candidate log[commit index]== fellower log[commit_index]
+            return currentTerm,True
+    return currentTerm,False
 
 
 
-flag_follower=False
-flag_leader=False
-flag_crash=False
-flag_candidate=False
-period_heartbeat=0.25##s
-period_election=0.6 ##s
-reset_timer=0
+
+
+def tester_getversion(filename):
+    return fileinfomap[filename][0]
+
+# Reads the config file and return host, port and store list of other servers
+def readconfig(config, servernum):
+    """Reads cofig file"""
+    fd = open(config, 'r')
+    l = fd.readline()
+
+    maxnum = int(l.strip().split(' ')[1])
+
+    if servernum >= maxnum or servernum < 0:
+        raise Exception('Server number out of range.')
+
+    d = fd.read()
+    d = d.splitlines()
+
+    for i in range(len(d)):
+        hostport = d[i].strip().split(' ')[1]
+        if i == servernum:
+            host = hostport.split(':')[0]
+            port = int(hostport.split(':')[1])
+
+        else:
+            serverlist.append(hostport)
+
+
+    return maxnum, host, port
 ##
+
+
 
 if __name__ == "__main__":
     try:
@@ -61,6 +186,32 @@ if __name__ == "__main__":
 
         fileinfomap = dict()#filename return [version, hashlist]
 
+        ####
+        ### parameter
+        currentTerm = 0
+        votedFor = None
+        id = 0  ##for example
+        log = []  ##[term, index,] ,filename, version, hashlist depend on myself
+        log.append([0, 0, "0", 0, 0])
+        commitIndex = 0  ## current log index
+        lastApplied = 0  ## applied in state machine
+        nextIndex = [0, 0, 0, 0, 0]  ## the index is not fixed????
+        matchIndex = [1, 1, 1, 1,
+                      1]  ## the value is matchIndex 2,3,1,2,3, for example, if the leader is 0, then it means fellower 1 have replicated 3 entries
+        ## and the fellower 2 have replicated 1 entires
+        flag_follower = True
+        flag_leader = False
+        flag_crash = False
+        flag_candidate = False
+        period_heartbeat = 0.25  ##s
+        period_election = 1  ##s
+        reset_timer = 0  ###????
+        follower_time_out = threading.Event()
+        ##
+        ##
+
+
+
         print("Attempting to start XML-RPC Server...")
         print(host, port)
         server = threadedXMLRPCServer((host, port), requestHandler=RequestHandler)
@@ -77,7 +228,7 @@ if __name__ == "__main__":
         server.register_function(restore,"surfstore.restore")
         server.register_function(isCrashed,"surfstore.isCrashed")
         server.register_function(requestVote,"surfstore.requestVote")
-        server.register_function(appendEntries,"surfstore.appendEntries")
+        server.register_function(append_Entries,"surfstore.append_Entries")
         server.register_function(tester_getversion,"surfstore.tester_getversion")
         print("Started successfully.")
         print("Accepting requests. (Halt program to stop.)")
@@ -103,90 +254,153 @@ class _thread_as_server (threading.Thread):
         logging.debug("Thread: %s start", (self.name))
         server.serve_forever()
 
+
 class _thread_leader_fellower (threading.Thread):
     def __init__(self, ):
         threading.Thread.__init__(self)
+        self.lock = threading.Lock()
+        self.leader_action_finished=threading.Event()
     def run(self):
         # logging.debug('event set: %s', event_is_set)
         logging.debug("Thread: %s start" , (self.name))
         while True:
             if isLeader():
-                leader_action()## don't finished until it is not the leader
+                thread_leader_action=threading.Thread(name='thread_leader_action',
+                                target=self.leader_action)
+                thread_leader_action.start()
+                leader_action_finished.wait()## don't finished until all the leader threads is finished
 
             if iscrash():
-                crash_action()
+                self.crash_action()## don't finish until it is not the crash
 
             if isfollower():
-                follower_action()
+                self.follower_action()
 
             if iscandidate():
-                candidate_action()
+                self.candidate_action()
+    def follower_action(self):
+
+        threading.Timer(period_election, self.follower_timeout,
+                        (follower, leader_term)).start()
+        follower_time_out.wait()
+        while flag_follower:
+            time.sleep(0.01)
+
+    def follower_timeout(self):
+        global  flag_follower
+        global  flag_candidate
+        flag_candidate=1
+        flag_follower=0
+        follower_time_out.set()
 
     def leader_action(self):
+
         global nextid
         global match_index
         nextid = [commitIndex + 1, commitIndex + 1, commitIndex + 1, commitIndex + 1, commitIndex + 1]
         match_index = [0, 0, 0, 0, 0]
-
-        leader_threads = {}
-        time_signals = {}
+        all_threads=[]
+        client_signals=[]
         for follower in followers:
-            prevlogindex = nextid[follower[0]] - 1
-            prev_log = log[prevlogindex]
-            follower_signal = threading.Event()
-            time_signals[follower[0]] = follower_signal
-            if nextid[follower[0]] >= len(log):
-                entries = []
-            else:
-                entries = log[nextid[follower[0]]]
-            thread_append = threading.Thread(name='periodically append',
-                                             target=append_entries_to_follower,
-                                             args=(follower_signal, follower, prevlogindex, prev_log, entries, currentTerm))
-            thread_append.setDaemon(True)
+            client_signal=threading.Event()
+            client_signals.append(client_signal)
+            thread_append = threading.Thread(name='heartbeat_periodically',
+                                             target=heartbeat_periodically,
+                                             args=( follower,leader_term))
+            thread_client_push = threading.Thread(name='client_push',
+                                             target=client_push,
+                                             args=( client_signal,follower,leader_term))
             thread_append.start()
-            thread_append_follower = []
-            thread_append_follower.append(thread_append)
-            thread_control = threading.Timer(period_heartbeat, leader_signal_periodically, (follower_signal,))
-            thread_control.setDaemon(True)
-            thread_control.start()
-            thread_append_follower.append(thread_control)
-            leader_threads[follower[0]] = thread_append_follower
+            thread_client_push.start()
+            all_threads.append(thread_append)
+            all_threads.append(thread_client_push)
             ## leader_threads is the list of [thread_append, thread_control]
 
-        while flag_leader:
-            pass
+        while flag_leader==1:
+            time.sleep(0.01)
+        for client_signal in client_signals:
+            client_signal.set()
+        for thread in all_threads: ## cannot release directly, must wait at least one heartbeat
+            thread.join()
+        self.leader_action_finished.set()  ##how to make leader finished, 1. the follower term, 2. rpc call
 
-
-    def leader_signal_periodically(self,time_signal):
-
-        time_signal.set()
-        threading.Timer(period_heartbeat, leader_signal_periodically, (follower_signal,)).start()
-
-    def append_entries_to_follower(self,time_signal,follower,prevlogindex, prev_log,entries,leader_term ):
+    def client_push(self,client_signal,follower,leader_term):
         global flag_leader
         global nextid
         global match_index
-        time_signal.wait()
-        follower_rpc = follower[1]
-        flag,match_index,follower_term=follower_rpc.append_Entries(prevlogindex, prev_log, entrie, leader_term)
-        time_signal.clear()
-        ###judge the case:
-        if leader_term <follower_term:
-            flag_leader=0
-            flag_follower=1
-            global currentTerm
-            currentTerm=follower_term
+        global flag_follower
+        client_signal.wait()### when we close the leader, we need to set the client_signal
+        if flag_leader==0:
+            logging.debug("stop leader running")
             return
-
-        if flag==False:
-            nextid[follower[0]]=nextid[follower[0]] - 1
+        prevlogindex = nextid[follower[0]] - 1
+        prev_log = log[prevlogindex]
+        if nextid[follower[0]] >= len(log):
+            entries = []
         else:
-            match_index[follower[0]]=match_index
+            entries = log[nextid[follower[0]]]
+        follower_rpc = follower[1]
+        flag, match_index, follower_term = follower_rpc.append_Entries(prevlogindex, prev_log, entries, leader_term)
+        ###judge the case:
+        self.lock.acquire()
+        if leader_term < follower_term:
+            flag_leader = 0
+            flag_follower = 1
+            global currentTerm
+            currentTerm = follower_term
+            return
+        ### change the global parameter
+
+        if flag == False:
+            nextid[follower[0]] = nextid[follower[0]] - 1
+        else:
+            match_index[follower[0]] = match_index
+        self.lock.release()
         ##
+        client_signal.clear()
+        threading.Thread(name='client_push',target=self.client_push,
+                        args=(client_signal, follower, leader_term)).start()
+
+
+
+    def heartbeat_periodically(self,follower,leader_term):##
+        global flag_leader
+        global nextid
+        global match_index
+        global flag_follower
+        global log
+        if flag_leader == 0:
+            ## if it doesn't run well, using global variable
+            return
 
         prevlogindex = nextid[follower[0]] - 1
         prev_log = log[prevlogindex]
-        self.append_entries_to_follower(time_signal, follower, prevlogindex, prev_log, entries, leader_term)
+        if nextid[follower[0]] >= len(log):
+            entries = []
+        else:
+            entries = log[nextid[follower[0]]]
+        follower_rpc = follower[1]
+        flag, match_index, follower_term = follower_rpc.append_Entries(prevlogindex, prev_log, entries, leader_term)
+        ###judge the case:
+        self.lock.acquire()
+        if leader_term < follower_term:
+            flag_leader = 0
+            flag_follower = 1
+            global currentTerm
+            currentTerm = follower_term
+            return
+        ### change the global parameter
+
+        if flag == False:
+            nextid[follower[0]] = nextid[follower[0]] - 1
+        else:
+            match_index[follower[0]] = match_index
+        self.lock.release()
+        ##
+
+        ### do that again
+        threading.Timer(period_heartbeat, self.heartbeat_periodically,
+                        ( follower,leader_term)).start()
 
 
 
@@ -236,3 +450,5 @@ def append_Entries(prevlogindex, prev_log, entries, leader_term):
                 reset_timer = 1  ##reset the timer
                 return True, match_index, term
 
+## the next task I should do is to how to initiate the parameter, follower,
+## and identify the function is right
